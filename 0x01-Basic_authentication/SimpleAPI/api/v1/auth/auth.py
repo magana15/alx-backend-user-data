cf6e1ab4d99195d805f6b3fca933972f76b59104
joel magana <joelmagana15@gmail.com>
2024-09-03 17:01:54 +0300

    authentic

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+
+from flask import request
+class Auth:
+    """
+
+    """
+
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        """
+        return
+
+
+    def authorization_header(self, request=None) -> None:
+        """
+
+        """
+        return 
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
+
